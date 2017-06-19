ebf44f4e122ce2c863fc1228c5b613197d220480
lzq100123 <lzq100123@qq.com>
2017-06-19 15:32:02 -0500

    update bmTestHelper

--- test/test.js
+++ test/test.js
@@ -2,16 +2,14 @@ const assert = require('assert');
 var buf = require('./BM_BufferPool');
 var bpe = new buf();
 var bp = new buf();
-bpe.fixCount = [1,2,3];
-bpe.dirty = [0,0,1];
-bpe.storage_page_map = [4,5,6];
-var str = ['[1 0 3],[2 0 2],[3 0 1]','[2 1 2],[3 1 3],[4 1 4]'];
+bp.fixCount = [2,3,4];
+bp.dirty = ['x',' ','x'];
+bp.storage_page_map = [2,3,4];
+var str = ['[1 3],[2 2],[3 1]','[2x2],[3 3],[4x4]'];
 var buflength = 3;
 var bufindex = buflength - 1; 
-i = 0;
-bp.fixCount = [];
-bp.dirty = [];
-bp.storage_page_map = [];
+var i = 0;
+
 
     /**
      * Return if the intput data fits the expect result?
@@ -19,41 +17,55 @@ bp.storage_page_map = [];
      * @param {BM_BufferPool} bp -- buffer pool
      * @param {string} [str=[i]]  --expect result in a moment i
      */
-    function bmTestHelper(bp, str[i]){
-        return;
-    }
-function FIFOtest(bp,i){
-    var count = 0;
-    for(var op in str){
-        var array = str[op].split(',');
-        for(var key1 in array){
-            var subarray = array[key1].split(',');
-            for(var key2 in subarray){
-                var subarray2 = subarray[key2].split('');
-                if(count > bufindex)
-                    count = 0;
-                bp.storage_page_map[count] = subarray2[1];
-                bp.dirty[count] = subarray2[3];
-                bp.fixCount[count] = subarray2[5];
+    function bmTestHelper(bp, str,i){
+        var count = 0;
+        var array = str[i].split(',');
+        for(var key in array){
+            var subarray = array[key].split('');
+            if(bp.storage_page_map[key] == subarray[1] 
+            && bp.dirty[key] == subarray[2] 
+            && bp.fixCount[key] == subarray[3]){
                 count++;
             }
         }
-        if(op == i)
-            break;
+        if(count == buflength)
+            return true;
+        return false;
     }
-}
-    describe('test',function(){
-        FIFOtest(bp,i);
-        it('test1',function(){
-            assert(3,bp.fixCount[1]);
-        })
-        it('test2',function(){
-            assert(1,bp.dirty[1]);
-        })
-        it('test3',function(){
-            assert(2,bp.storage_page_map[0]);
-        })
-    })
+
+
+    console.log(bmTestHelper(bp,str,i));
+    // var count = 0;
+    // for(var op in str){
+    //     var array = str[op].split(',');
+    //     for(var key1 in array){
+    //         var subarray = array[key1].split(',');
+    //         for(var key2 in subarray){
+    //             var subarray2 = subarray[key2].split('');
+    //             if(count > bufindex)
+    //                 count = 0;
+    //             bp.storage_page_map[count] = subarray2[1];
+    //             bp.dirty[count] = subarray2[3];
+    //             bp.fixCount[count] = subarray2[5];
+    //             count++;
+    //         }
+    //     }
+    //     if(op == i)
+    //         break;
+    // }
+
+    // describe('test',function(){
+    //     FIFOtest(bp,i);
+    //     it('test1',function(){
+    //         assert(3,bp.fixCount[1]);
+    //     })
+    //     it('test2',function(){
+    //         assert(1,bp.dirty[1]);
+    //     })
+    //     it('test3',function(){
+    //         assert(2,bp.storage_page_map[0]);
+    //     })
+    // })
     
 
 
